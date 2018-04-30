5357d1268d125204c2d3d704e0082867b0cecc2b
CRefice <CRefice@gmail.com>
2018-04-30 17:46:52 +0200

    Rectify wrong expected translation results

--- tests/transform.cpp
+++ tests/transform.cpp
@@ -31,12 +31,12 @@ TEST_CASE( "transformations", "[transform]" ) {
 	SECTION( "translation" ) {
 		ssm::vec3 pos(3.0f, 4.0f, 5.0f);
 		ssm::mat4 mat = ssm::translation(pos);
-		ssm::vec4 vec(1.0f, 2.0f, 3.0f, 4.0f);
+		ssm::vec4 vec(1.0f, 2.0f, 3.0f, 1.0f);
 
 		ssm::vec4 vec_2 = mat * vec;
 		REQUIRE(epsilon_compare(vec_2.x, 4.0f, 1e-6f));
 		REQUIRE(epsilon_compare(vec_2.y, 6.0f, 1e-6f));
 		REQUIRE(epsilon_compare(vec_2.z, 8.0f, 1e-6f));
-		REQUIRE(epsilon_compare(vec_2.z, 4.0f, 1e-6f));
+		REQUIRE(epsilon_compare(vec_2.w, 1.0f, 1e-6f));
 	}
 }
